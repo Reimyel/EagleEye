caf84fde77184ac98a4bcf39d60511258fe84548
Reimyel <caiquegsambrana@gmail.com>
2025-04-23 18:23:53 -0300

    b

--- 3D Horror/Assets/_Game/_Scripts/Camera/Move.cs
+++ 3D Horror/Assets/_Game/_Scripts/Camera/Move.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public float MouseSensitivity = 200f;
-    public Transform PlayerBody;
+    public Transform Player;
     float _xRotation = 0f;
 
     void Update()
@@ -14,13 +14,13 @@ public class Move : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
 
+        //cima e baixo
         _xRotation -= mouseY;
         _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
 
-        //cima e baixo
         transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
 
         //esquerda e direita
-        PlayerBody.Rotate(Vector3.up * mouseX);
+        Player.Rotate(Vector3.up * mouseX);
     }
 }
