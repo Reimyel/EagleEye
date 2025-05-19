6a2ba266c4d79f8497201095afbc3c239d4f33c8
m-duca <theu.duka@gmail.com>
2025-05-19 17:25:34 -0300

    Revert "style: heropropchair new intro"
    This reverts commit 3a95712cf339d186fb1995b43e1de348a5d78d54.

--- Eagle Eye/Assets/_Game/_Scripts/Entities/HeroProps/HeroPropChairOffice.cs
+++ Eagle Eye/Assets/_Game/_Scripts/Entities/HeroProps/HeroPropChairOffice.cs
@@ -20,12 +20,17 @@ namespace FourZeroFourStudios
         [SerializeField] GameObject _go_canvas_hud;
         [SerializeField] PostBehaviours _postBehaviours;
 
+
         [Header("Rendering:")]
         [SerializeField] Volume _volume;
         [SerializeField] VolumeProfile _vprofile_crt;
         [SerializeField] float _changeProfileDelay;
 
+        Animator _anim_cameraHolder;
+
         bool _isFirstTime = true;
+
+        void Start() => _anim_cameraHolder = _cameraHolder.gameObject.GetComponent<Animator>();
 
         public override void Interact()
         {
@@ -37,25 +42,33 @@ namespace FourZeroFourStudios
             _cameraHolder.gameObject.transform.position = _transf_cameraPosition.position;
             _cameraHolder.gameObject.transform.rotation = _transf_cameraPosition.rotation;
 
+            _anim_cameraHolder.Play("Anim_CameraHolder_ZoomIn");
+
             _go_canvas_hud.SetActive(false);
 
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
 
-            ApplyCrt();
+            StartCoroutine(ApplyCrt());
         }
 
-        void ApplyCrt() 
+        IEnumerator ApplyCrt() 
         {
+            yield return new WaitForSeconds(_changeProfileDelay);
+
             _volume.profile = _vprofile_crt;
 
             _go_desktop_canvas.SetActive(true);
             _go_initial_canvas.SetActive(true);
 
             if (_isFirstTime)
+            {
                 _isFirstTime = false;
+            }
             else
+            {
                 _postBehaviours.ReturnToPosts();
+            }
 
             this.enabled = false;
         }
