27b984e6f276f0fbe434f2a0077d95bfa891b196
Reimyel <caiquegsambrana@gmail.com>
2025-05-25 16:07:10 -0300

    feat: drinking coffee anim

--- Eagle Eye/Assets/_Game/_Scripts/Entities/Props/PropCoffeeCup.cs
+++ Eagle Eye/Assets/_Game/_Scripts/Entities/Props/PropCoffeeCup.cs
@@ -6,16 +6,33 @@ namespace FourZeroFourStudios
 {
     public class PropCoffeeCup : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        [SerializeField] Animator _anim_coffeeCup;
+        bool _canDrink = true;
+        int _timesDrank = 0;
 
-        // Update is called once per frame
         void Update()
         {
-        
+            if (Input.GetMouseButtonDown(0) && _canDrink)
+            {
+                _anim_coffeeCup.SetBool("_isDrinking", true);
+                _canDrink = false;
+                StartCoroutine(AnimDefault(2));
+                
+            }
+
+            //discard coffee cup
+            if (_timesDrank >= 3)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        IEnumerator AnimDefault(float _delayInSeconds)
+        {
+            yield return new WaitForSeconds(_delayInSeconds);
+            _anim_coffeeCup.SetBool("_isDrinking", false);
+            _timesDrank++;
+            _canDrink = true;
         }
     }
 }
