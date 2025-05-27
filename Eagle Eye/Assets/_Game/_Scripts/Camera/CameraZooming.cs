de0175abcd29d5735cd27e89d6054ad4c68cb304
m-duca <theu.duka@gmail.com>
2025-05-27 12:40:41 -0300

    feat: camera zoom speed system

--- Eagle Eye/Assets/_Game/_Scripts/Camera/CameraZooming.cs
+++ Eagle Eye/Assets/_Game/_Scripts/Camera/CameraZooming.cs
@@ -6,28 +6,60 @@ namespace FourZeroFourStudios
 {
     public class CameraZooming : MonoBehaviour
     {
+        #region Members
+        
+        [Header("References:")]
         [SerializeField] Animator _anim_camHolder;
+
+        [Header("Parameters:")]
+        [SerializeField] float _speedIn;
+        [SerializeField] float _speedOut;
         
         bool _canZooming = true;
+        float _defaultAnimSpeed;
+        #endregion
+
+        #region Mono
+        void Start() => _defaultAnimSpeed = _anim_camHolder.speed;
 
         void Update()
         {
             CheckZoomIn();
             CheckZoomOut();
         }
+        #endregion
 
+        #region Custom
+
+        #region Inputs
         void CheckZoomIn() 
         {
-            if (Input.GetButtonDown("Zoom") && _canZooming)
+            if (Input.GetButtonDown("Zoom") && _canZooming) 
+            {
+                StopAllCoroutines();
+                _anim_camHolder.speed = _speedIn;
                 _anim_camHolder.Play("Anim_CameraHolder_ZoomIn");
+            }
         }
 
         void CheckZoomOut() 
         {
-            if (Input.GetMouseButtonUp(1))
+            if (Input.GetButtonUp("Zoom")) 
+            {
+                _anim_camHolder.speed = _speedOut;
                 _anim_camHolder.Play("Anim_CameraHolder_ZoomOut");
+                StartCoroutine(ResetAnimatorSpeed(3.0f));
+            }
         }
 
+        IEnumerator ResetAnimatorSpeed(float value) 
+        {
+            yield return new WaitForSeconds(value);
+            _anim_camHolder.speed = _defaultAnimSpeed;
+        }
+        #endregion
+
+        #region Managing Behaviour
         public void Disactivate() 
         {
             _canZooming = false;
@@ -35,5 +67,8 @@ namespace FourZeroFourStudios
         }
 
         public void Activate() => _canZooming = true;
+        #endregion
+
+        #endregion
     }
 }
