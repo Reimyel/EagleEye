edf03739fd29b7604c09d87fc827c5869bdaa731
m-duca <theu.duka@gmail.com>
2025-06-18 18:17:53 -0300

    feat: turnstile sfx implementation

--- Eagle Eye/Assets/_Game/_Scripts/Triggers/TriggerRotateDoor.cs
+++ Eagle Eye/Assets/_Game/_Scripts/Triggers/TriggerRotateDoor.cs
@@ -13,11 +13,21 @@ namespace FourZeroFourStudios
         [Header("Settings:")]
         [Space]
 
+        [Header("Parameters:")]
+        [SerializeField] float _sfxInterval;
+        [Space]
+
         [Header("References:")]
         [SerializeField] Transform _transf_door;
+        [SerializeField] EntitySFXController _sfxController;
+        [Space]
 
         [Header("Parameters:")]
         [SerializeField] float _rotateSpeed;
+        [Space]
+
+        // SFX Control
+        bool _isPlayingSFX = false;
         #endregion
 
         #region Unity
@@ -29,13 +39,36 @@ namespace FourZeroFourStudios
         #region Custom
         void CheckRotate(GameObject gameObjectValue, TextMeshProUGUI tmpValue) 
         {
-            if (gameObjectValue != gameObject) return;
+            if (gameObjectValue != gameObject)
+            {
+                if (_isPlayingSFX) 
+                {
+                    _isPlayingSFX = false;
+                    StopAllCoroutines();
+                }
+                return;
+            }
 
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.W)) 
+            {
+                if (!_isPlayingSFX) 
+                {
+                    StartCoroutine(PlayRotating());
+                    _isPlayingSFX = true;
+                }
+
                 Rotate();
+            }
         }
 
         void Rotate() => _transf_door.rotation *= Quaternion.Euler(0f, -_rotateSpeed * Time.deltaTime, 0f);
+        
+        IEnumerator PlayRotating() 
+        {
+            _sfxController.Play("Rotate");
+            yield return new WaitForSeconds(_sfxInterval);
+            StartCoroutine(PlayRotating()); 
+        }
         #endregion
     }
 }
